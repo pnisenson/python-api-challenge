736a2e17efe3c4bf0525401061c03e845def918a
pnisenson <pnisenson@gmail.com>
2020-12-17 17:13:13 -0500

    began VacationPy

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "YOUR KEY HERE!"
+
+# Google API Key
+g_key = "AIzaSyD_ylG-_w9jbNbfuTV4NbLEnG6lnKIuYC8"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "fe2b243087638dbb02098a344bc12cc9"
+
+
+
